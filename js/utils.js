6f5aa14c9956bd4d1d3e286290432cbf35aa016c
lizhongwu <1289205959@qq.com>
2016-04-29 16:44:19 +0800

    最终版本

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,18 @@
-var utils={listToArray:function(likeAry){var ary=[];try{ary=Array.prototype.slice.call(likeAry)}catch(e){for(var i=0;i<likeAry.length;i++){ary[ary.length]=likeAry[i]}}return ary},jsonParse:function(str){return"JSON"in window?JSON.parse(str):eval("("+str+")")}}
+var utils = {
+    //实现将类数组转换为数组
+    listToArray: function (likeAry) {
+        var ary = [];
+        try {
+            ary = Array.prototype.slice.call(likeAry);
+        } catch (e) {
+            for (var i = 0; i < likeAry.length; i++) {
+                ary[ary.length] = likeAry[i];
+            }
+        }
+        return ary;
+    },
+
+    jsonParse : function(str) {
+       return "JSON" in window ? JSON.parse(str) : eval("("+str+")");
+    }
+}
